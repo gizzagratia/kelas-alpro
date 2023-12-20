8c2d9ce312773e6f9e54bdafb59d27c05ac88599
root <gizzagratia@gmail.com>
2023-12-20 11:16:27 +0700

    file pencarian

--- Tugas10-AlproPencarian/pencarian1.c
+++ Tugas10-AlproPencarian/pencarian1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #define SIZE 100
 
 int x;
@@ -8,18 +9,24 @@ int main(void){
     int a[SIZE];
     for (size_t x = 0; x < SIZE; ++x)
     {
-        a[x] = 2 * x;
+        a[x] = 2 * x + 1;
     }
-    printf("Enter integer search key: ");
+    printf("Enter integer search key : ");
     int searchKey;
     scanf("%d", &searchKey);
 
+    clock_t begin = clock();
     size_t index = linearSearch(a, searchKey, SIZE);
+    clock_t end = clock();
+
     if (index != -1){
         printf("Found value at index %ld\n", index);
     }else{
         puts("Value not found");
     }
+    
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Waktu yang dibutuhkan %f detik\n\n", time_spent);
 }
 
     size_t linearSearch(const int array[], int key, size_t size){
---
+++ Tugas10-AlproPencarian/tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Waktu yang dibutuhkan %f detik\n\n", time_spent);
